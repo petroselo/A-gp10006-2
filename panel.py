Fix nhat sign

panel.py
@@ -35,12 +35,12 @@ def solve_panels(xv, yv, U, pitch=0):
 				ainf[ti, vi] = ( 1/(2*np.pi*r2) ) * (-nhatx[ti]*Δy  + nhaty[ti]*Δx )
 
 	else: # Infinite Cascade
-		pitch = BC_FLAG
+		
 		pass
 
     # Apply the kutta condition that vorticity at the final point = -(vorticity at the first point.) => no vorticity. 
-	ainf[nv-1,0] = 1/dl[0]
-	ainf[nv-1,nv-1] = 1/dl[nv-2]
+	ainf[nv-1, 0] = 1/dl[0]
+	ainf[nv-1, nv-1] = 1/dl[nv-2]
 
      # Solve ainf*g=-u_ind for gam
 	try:
@@ -66,6 +66,6 @@ def target_points(xv, yv):
 	dl = np.sqrt(δx*δx + δy*δy)
 
 	nhatx = δy / dl
-	nhaty = δx / dl
+	nhaty = -δx / dl
 
 	return xt, yt, dl, nhatx, nhaty

test.py
@@ -9,21 +9,21 @@ knot_points = [ [600,300], [500,350], [300,350], [250,285], [300,300], [500,300]
 knot_points.append(knot_points[0])
 knots = np.array(knot_points) + 0.0002# n x 2
 
-_, visual_spline_points = splinefit(knots, 100)
+_, visual_spline_points = splinefit(knots, 200)
 
 # Convert knots to uniform spaced vortices. There are n-1 panels as two vortices at trailing edge.
-n_vort = 400
+n_vort = 100
 _, hi_res_spline_points = splinefit(knots, 5*n_vort)
 vortex_points = uniform_points(hi_res_spline_points, n_vort)
 
-print(vortex_points)
+#print(vortex_points)
 
 U = np.array([1,0])
 
 # Vortex circulations
 gam = solve_panels(vortex_points[:,0], vortex_points[:,1], U)
 
-#print(gam)
+print(gam)
 
 xvec, yvec = np.linspace(0, 800, 101), np.linspace(0, 600, 101)
 x, y = np.meshgrid(xvec, yvec, indexing='ij')
@@ -41,5 +41,5 @@ for i in range(n_vort):
 plt.plot(visual_spline_points[:,0], visual_spline_points[:,1])
 plt.plot(knots[:,0], knots[:,1], 'or')
 plt.plot(vortex_points[:,0], vortex_points[:,1], 'ok', scalex=0.001, scaley=0.001)
-plt.contourf(x, y, ψ, levels=np.linspace(np.min(ψ), np.max(ψ), 40), antialiased=False, linestyles='solid')
+plt.contourf(x, y, ψ, levels=np.linspace(np.min(ψ), np.max(ψ), 60), antialiased=False, linestyles='solid')
 plt.show()